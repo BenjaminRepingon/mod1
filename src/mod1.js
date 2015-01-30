d69b683778ac6b603fde6a09716581e0f3d3e456
BenjaminRepingon <repingonbenjamin@gmail.com>
2015-01-30 11:58:50 +0100

    [ADD]

--- src/mod1.js
+++ src/mod1.js
@@ -1,29 +1,99 @@
 MOD1.scene = function( scene )
 {
-	// This creates and positions a free camera (non-mesh)
 	var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-
-	// This targets the camera to scene origin
 	camera.setTarget(BABYLON.Vector3.Zero());
-
-	// This attaches the camera to the canvas
 	camera.attachControl(canvas, true);
 
-	// This creates a light, aiming 0,1,0 - to the sky (non-mesh)
 	var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-	var light0 = new BABYLON.PointLight("Omni0", new BABYLON.Vector3(-1, 1, -1), scene);
-	light0.diffuse = new BABYLON.Color3(1, 0, 0);
-	light0.specular = new BABYLON.Color3(1, 1, 1);
-
-	// Default intensity is 1. Let's dim the light a small amount
 	light.intensity = 0.7;
+	// var light0 = new BABYLON.PointLight("Omni0", new BABYLON.Vector3(-1, 1, -1), scene);
+	// light0.diffuse = new BABYLON.Color3(1, 0, 0);
+	// light0.specular = new BABYLON.Color3(1, 1, 1);
 
-	// Our built-in 'sphere' shape. Params: name, subdivs, size, scene
-	var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
+	var ground = new BABYLON.Mesh.CreateGround( "ground", 10, 10, 2, scene );
 
-	// Move the sphere upward 1/2 its height
-	sphere.position.y = 1;
+	// Emitters
+	var emitter0 = BABYLON.Mesh.CreateBox("emitter0", 0.1, scene);
+	emitter0.position.y = 10;
+	emitter0.isVisible = false;
 
-	// Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
-	var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
+	// Custom shader for particles
+	// BABYLON.Effect.ShadersStore["myParticleFragmentShader"] =
+	// "#ifdef GL_ES\n" +
+	// "precision highp float;\n" +
+	// "#endif\n" +
+
+	// "varying vec2 vUV;\n" +                     // Provided by babylon.js
+	// "varying vec4 vColor;\n" +                  // Provided by babylon.js
+
+	// "uniform sampler2D diffuseSampler;\n" +     // Provided by babylon.js
+	// "uniform float time;\n" +                   // This one is custom so we need to declare it to the effect
+
+	// "void main(void) {\n" +
+	// 	"vec2 position = vUV;\n" +
+
+	// 	"float color = 0.0;\n" +
+	// 	"vec2 center = vec2(0.5, 0.5);\n" +
+
+	// 	"color = sin(distance(position, center) * 10.0+ time * vColor.g);\n" +
+
+	// 	"vec4 baseColor = texture2D(diffuseSampler, vUV);\n" +
+
+	// 	"gl_FragColor = baseColor * vColor * vec4( vec3(color, color, color), 1.0 );\n" +
+	// "}\n" +
+	// "";
+
+	// // Effect
+	// var effect = engine.createEffectForParticles("myParticle", ["time"]);
+
+	// Particles
+	var defaultRadius = 0.5;
+	var particleSystem = new BABYLON.ParticleSystem("particles", 100, scene/*, effect*/);
+	particleSystem.particleTexture = new BABYLON.Texture("textures/flare.png", scene);
+	particleSystem.minSize = 0.1;
+	particleSystem.maxSize = 1.0;
+	particleSystem.minLifeTime = 0.5;
+	particleSystem.maxLifeTime = 5.0;
+	particleSystem.minEmitPower = 0.5;
+	particleSystem.maxEmitPower = 3.0;
+	particleSystem.emitter = emitter0;
+	particleSystem.emitRate = 10;
+	particleSystem.blendMode = BABYLON.ParticleSystem.BLENDMODE_ONEONE;
+	particleSystem.direction1 = new BABYLON.Vector3(-1, 1, -1);
+	particleSystem.direction2 = new BABYLON.Vector3(1, 1, 1);
+	particleSystem.color1 = new BABYLON.Color4(0, 0.5, 1, 1);
+	particleSystem.color2 = new BABYLON.Color4(0, 0.2, 0.5, 1);
+	particleSystem.gravity = new BABYLON.Vector3(0, -10.0, 0);
+	particleSystem.start();
+
+	particleSystem.updateFunction = function ( particles )
+	{
+		for ( var index = 0; index < particles.length; index++ )
+		{
+			var particle = particles[index];
+
+			console.log( "pok" )
+
+			if ( particle.position.y - defaultRadius <= ground.position.y )
+				particle.direction.y = 0;
+		}
+	};
+
+	// var time = 0;
+	// var order = 0.1;
+
+	// scene.registerBeforeRender(function () {
+	// 	// Waiting for effect to be compiled
+	// 	if (!effect) {
+	// 		return;
+	// 	}
+
+	// 	effect.setFloat("time", time);
+
+	// 	time += order;
+
+	// 	if (time > 100 || time < 0) {
+	// 		order *= -1;
+	// 	}
+	// });
 }
